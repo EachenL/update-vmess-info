a15dab488dd393e8c400426c8dcc13639b13a9ab
Ning Mark <shundyning@outlook.com@labnet-3.olccenvk3ozexlbgsvajvgseph.lx.internal.cloudapp.net>
2023-11-22 14:27:01 +0000

    transfer to labnet-3

--- get_and_push_vmess.sh
+++ get_and_push_vmess.sh
@@ -2,12 +2,12 @@ vmesses=$(./get_vmesses.sh)
 echo "$vmesses"
 vmesses_for_subconverter=$(echo "$vmesses" | tr '\n' '\|' | sed 's/|$//')
 echo $vmesses_for_subconverter
-cd /home/ning/labnet-sub
+cd /home/shundyning/labnet-sub
 git pull
 echo "http://127.0.0.1:25500/sub?target=clash&url=${after_echo}"
 cleaned_url=$(echo -e "$vmesses_for_subconverter" | sed 's/\x1b\[[0-9;]*m//g')
-nohup /home/ning/subconverter/subconverter &
-wget -O /home/ning/labnet-sub/subcriptions/subscriptions "http://127.0.0.1:25500/sub?target=clash&url=${cleaned_url}"
+nohup /home/shundyning/subconverter/subconverter &
+wget -O /home/shundyning/labnet-sub/subcriptions/subscriptions "http://127.0.0.1:25500/sub?target=clash&url=${cleaned_url}"
 git add .
 git commit -m "update"
 git push

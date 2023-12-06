fdd95bc61afa66c4dcd77f65011143ccac18685b
Ning Mark <shundyning@outlook.com@labchat-jp-3.51ufyvohk5mujflnczb1rmfiid.lx.internal.cloudapp.net>
2023-12-06 09:35:24 +0000

    multi server modify

--- update_vpn.sh
+++ update_vpn.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-/home/ning/update-vmess-info/update-vm-ips.sh
-/home/ning/update-vmess-info/get_and_push_vmess.sh
+UPDATE_VM_IPS_SCRIPT="$HOME/update-vmess-info/update-vm-ips.sh"
+GET_AND_PUSH_VMESS_SCRIPT="$HOME/update-vmess-info/get_and_push_vmess.sh"
+"$UPDATE_VM_IPS_SCRIPT"
+"$GET_AND_PUSH_VMESS_SCRIPT"
